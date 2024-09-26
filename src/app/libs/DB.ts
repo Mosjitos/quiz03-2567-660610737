333e43fbc87a736f7b8fabd57d6c2ba4bfae3bed
Mook <kamolrat_saeboon@cmu.ac.th>
2024-09-26 14:56:09 +0700

    Interface databasr

--- src/app/libs/DB.ts
+++ src/app/libs/DB.ts
@@ -19,6 +19,12 @@ export interface User {
   role: "ADMIN" | "SUPER_ADMIN";
 }
 
+export interface Database {
+  room: Room[];
+  message: Message[];
+  users: User[];
+}
+
 const originalDB = {
   rooms: [
     {
